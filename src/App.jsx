3ff688beb0fdd38504abdc1c8394f1282dc6b7e9
Shinichiro Ito <82028845+itoshin1341@users.noreply.github.com>
2022-04-10 21:35:32 +0900

    再レンダリング

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,25 @@ import React, { StrictMode, useState } from "react";
 import ColofulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColofulMessage color="blue">お元気ですか？</ColofulMessage>
       <ColofulMessage color="pink">元気です！</ColofulMessage>
-      <button onClick={onClickCountUp}>カウントアップ</button>
+      <button onClick={onClickCountUp}>カウントアップ！</button>
+      <br />
+      <button>on/off</button>
       <p>{num}</p>
+      <p>( ・∇・)</p>
     </>
   );
 };
 
 export default App;
+
+// Lesson47.　2min 22second より視聴する。
